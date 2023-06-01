4bd6661b7ba6250448d7ba582f2174236ff75995
pkbsa <siranut.poko@hotmail.com>
2023-06-01 13:08:45 +0700

    added menu page

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -61,6 +61,110 @@ var products = [
         category: "PYE VEGETARIAN DISHES",
         status: "Available"
     }),
+    new Product({
+        imagePath: ['/products/img7.png'],
+        title: 'Fried Tofu',
+        description: 'Fried tofu served with sweet chili sauce and crushed peanuts',
+        price: 6.5,
+        category: "PYE VEGETARIAN DISHES",
+        status: "Available"
+    }),
+    new Product({
+        imagePath: ['/products/img11.png'],
+        title: 'Boat Noodles with Pork',
+        description: 'Fresh pork blood broth with soy and anise, bean sprout,slice pork, pork meat ball top with celery and pork rind',
+        price: 14.5,
+        category: "PYE BOAT NOODLES",
+        status: "Available"
+    }),
+    new Product({
+        imagePath: ['/products/img12.png'],
+        title: 'Boat Noodles with Pork',
+        description: 'Fresh beef blood broth with soy and anise, bean sprout, slice beef, beef meat ball top with celery and pork rind',
+        price: 14.5,
+        category: "PYE BOAT NOODLES",
+        status: "Available"
+    }),
+    new Product({
+        imagePath: ['/products/img13.png'],
+        title: 'Tom Yum Bolarn Noodles',
+        description: 'Chicken and soy broth with lime juice, bean sprout, mince pork, fish cake, shrimp meat ball and fish meat ball top with cilantro and scallion (medium spicy)',
+        price: 14.5,
+        category: "PYE BOAT NOODLES",
+        status: "Available"
+    }),
+    new Product({
+        imagePath: ['/products/img14.png'],
+        title: 'Yen Ta Fo Noodles',
+        description: 'Chicken, soy broth with red bean paste, morning glory,shrimp,squid,white fungus,fish cake,shrimp meat ball and fish meat ball top with cilantro and scallion',
+        price: 14.5,
+        category: "PYE BOAT NOODLES",
+        status: "Available"
+    }),
+    new Product({
+        imagePath: ['/products/img15.png'],
+        title: 'Pad Kee Mao',
+        description: 'Sauteed wide ribbon noodles with Thai herbs, chili and mixed vegetables (spicy!!!) (Real Thai style kee mao!!!)',
+        price: 14.5,
+        category: "PYE DRY NOODLES",
+        status: "Available"
+    }),
+    new Product({
+        imagePath: ['/products/img16.png'],
+        title: 'Pad See Ew',
+        description: 'Sauteed wide ribbon noodles,egg with Chinese broccoli in sweet black soy sauce.',
+        price: 14.5,
+        category: "PYE DRY NOODLES",
+        status: "Available"
+    }),
+    new Product({
+        imagePath: ['/products/img17.png'],
+        title: 'Fried Tofu',
+        description: 'Fried tofu served with sweet chili sauce and crushed peanuts.',
+        price: 6.5,
+        category: "J SNACKS",
+        status: "Available"
+    }),
+    new Product({
+        imagePath: ['/products/img18.png'],
+        title: 'Taro Rolls',
+        description: 'Taro rolls served with sweet chili sauce.',
+        price: 5.5,
+        category: "J SNACKS",
+        status: "Available"
+    }),
+    new Product({
+        imagePath: ['/products/img19.png'],
+        title: 'Jae Noodle Soup',
+        description: 'Noodle soup in vegetable broth with Seitan and shitake mushrooms top with cilantro and scallion.',
+        price: 14,
+        category: "JAE NOODLES",
+        status: "Available"
+    }),
+    new Product({
+        imagePath: ['/products/img20.png'],
+        title: 'Tom Yum Jae',
+        description: 'Spicy and sour broth with Seitan and shitake mushrooms crushed peanuts top with cilantro and scallion.',
+        price: 14,
+        category: "JAE NOODLES",
+        status: "Available"
+    }),
+    new Product({
+        imagePath: ['/products/img21.png'],
+        title: 'Watermelon frozen',
+        description: ' ',
+        price: 7,
+        category: "DRINK",
+        status: "Available"
+    }),
+    new Product({
+        imagePath: ['/products/img22.png'],
+        title: 'Nam Keng Sai',
+        description: 'Shaved iced with mixed tropical fruits Choice of sweet red syrup and condense milk OR coconut milk infused with palm sugar',
+        price: 10,
+        category: "DESERT",
+        status: "Available"
+    }),
 ]
 
 var done = 0;
